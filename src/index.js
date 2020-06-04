2375299683ba432019686672a60a3113e99df29b
kavience <599513860@qq.com>
2020-06-04 10:16:37 +0800

    fix(pop): fix bestMatch.pop

--- src/index.js
+++ src/index.js
@@ -495,6 +495,10 @@ class MiniCssExtractPlugin {
           // no module found => there is a conflict
           // use list with fewest failed deps
           // and emit a warning
+          // TODO: if no bestMatch then break;
+          if (!bestMatch) {
+            break;
+          }
           const fallbackModule = bestMatch.pop();
 
           if (!this.options.ignoreOrder) {
